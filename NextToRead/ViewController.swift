2833b4b67b0e11ed4012b998e79265c3b0da0d93
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2020-10-05 21:10:04 +0300

    Initial Commit

---
+++ NextToRead/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextToRead
+//
+//  Created by Ekaterina Khudzhamkulova on 5.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
