e025125b0e715c873b5ac77cf87c137ef39bc7dd
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2020-10-06 23:06:16 +0300

    Create func saveCategories and addButtonPressed

--- NextToRead/Controllers/CategoryViewController.swift
+++ NextToRead/Controllers/CategoryViewController.swift
@@ -31,7 +31,25 @@ class CategoryViewController: UITableViewController {
     }
 
 	@IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-		saveCategories()
+		let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+		var nameField = UITextField()
+
+		let action = UIAlertAction(title: "Add", style: .default) { [self] (action) in
+			let newCategory = Category(context: self.context)
+			if let message = nameField.text {
+				newCategory.name = message
+				self.categories.append(newCategory)
+
+				saveCategories()
+			}
+		}
+		alert.addTextField { (textField) in
+			textField.placeholder = "Create New Category"
+			nameField = textField
+		}
+
+		alert.addAction(action)
+		present(alert, animated: true, completion: nil)
 	}
 
 	func loadCategories() {
@@ -44,17 +62,26 @@ class CategoryViewController: UITableViewController {
 		tableView.reloadData()
 	}
 
+	func saveCategories() {
+		do {
+			try context.save()
+		} catch {
+			print("Error saving categories, \(error)")
+		}
+		tableView.reloadData()
+	}
+
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return categoriesExmp.count
+		return categories.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: Constants.categoryCell.rawValue, for: indexPath)
 
-		cell.textLabel?.text = categoriesExmp[indexPath.row]
+		cell.textLabel?.text = categories[indexPath.row].name
         return cell
     }
 
