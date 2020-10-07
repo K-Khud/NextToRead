71f560d7acd16ec63a4cee215568d389e0555a78
Ekaterina Khudzhamkulova <katerina.koreneva@gmail.com>
2020-10-07 12:18:56 +0300

    Add func loadBooks

--- NextToRead/Controllers/ItemsViewController.swift
+++ NextToRead/Controllers/ItemsViewController.swift
@@ -9,14 +9,17 @@ import UIKit
 import CoreData
 
 class ItemsViewController: UITableViewController {
-	var selectedCategory: Category?
+	var selectedCategory: Category? {
+		didSet {
+			loadBooks()
+		}
+	}
+	var books = [Book]()
 	let booksExmp = ["One", "Two", "Three"]
+	let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
@@ -26,53 +29,29 @@ class ItemsViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-		return booksExmp.count
+		return books.count
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-		cell.textLabel?.text = booksExmp[indexPath.row]
+		let cell = tableView.dequeueReusableCell(withIdentifier: Constants.bookCell.rawValue, for: indexPath)
+		cell.textLabel?.text = books[indexPath.row].title
 
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+	func loadBooks() {
+		let request: NSFetchRequest<Book> = Book.fetchRequest()
+		guard let categoryArgument = selectedCategory?.name else {return}
+		let predicate = NSPredicate(format: "parentCategory.name MATCHES %@", categoryArgument)
+		request.predicate = predicate
+		do {
+			books = try context.fetch(request)
+		} catch {
+			print("Error fetching books, \(error)")
+		}
+		tableView.reloadData()
+	}
     /*
     // MARK: - Navigation
 
